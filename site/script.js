17c6f0fea1bdc2a328af8c6d428064e1ab4fc40e
Ruminini <63514002+Ruminini@users.noreply.github.com>
2025-11-01 14:44:41 -0300

    Update calendario and enhance event display in the portal
    - Added new academic events to calendario.json.
    - Modified index.html to display upcoming events in a list format.
    - Improved script.js to render events dynamically and highlight active ones.
    - Enhanced styles in style.css for better layout and visual distinction of events.

--- site/script.js
+++ site/script.js
@@ -9,13 +9,55 @@ async function loadData() {
 }
 
 function renderCalendario(data) {
-	const div = document.getElementById("calendario-content");
-	div.innerHTML = "";
-	data.eventos.forEach((e) => {
-		const card = document.createElement("div");
-		card.className = "card";
-		card.innerHTML = `<b>${e.fecha}</b> — ${e.descripcion}`;
-		div.appendChild(card);
+	const hoy = new Date();
+	const proximosEventos = document.getElementById("proximos-eventos");
+	let calendarioData = data.eventos;
+	calendarioData = calendarioData
+		.map((e) => {
+			const [d, m, y] = e.fecha.split("/").map(Number);
+			const date = new Date(y, m - 1, d);
+			const tieneFin = e["fechaFin"] ? true : false;
+			if (tieneFin) {
+				const [df, mf, yf] = e["fechaFin"].split("/").map(Number);
+				const endDate = new Date(yf, mf - 1, df);
+				return { ...e, date: date, endDate: endDate };
+			}
+			return { ...e, date: date };
+		})
+		.map((e) => {
+			return {
+				...e,
+				activo: e.date == hoy || (e.endDate >= hoy && e.date <= hoy),
+			};
+		})
+		.sort((a, b) => {
+			if ((a.activo || a.date > hoy) && (b.activo || b.date > hoy))
+				return a.date - b.date;
+			if (a.activo || a.date > hoy) return 1;
+			if (b.activo || b.date > hoy) return -1;
+			return (
+				(a.endDate ? a.endDate : a.date) - (b.endDate ? b.endDate : b.date)
+			);
+		});
+	proximosEventos.innerHTML = calendarioData.length
+		? calendarioData
+				.map(
+					(e) =>
+						`<div class="card ${e.activo ? "active" : ""} ${
+							e.date < hoy && !e.activo ? "old" : ""
+						}">${e.fecha.slice(0, -5)}${
+							e.fechaFin ? " - " + e.fechaFin.slice(0, -5) : ""
+						} — <b>${e.descripcion}</b></div>`
+				)
+				.join("")
+		: "<p>No hay eventos activos.</p>";
+
+	const events = proximosEventos.getElementsByClassName("card");
+	let nextEvent = proximosEventos.querySelector(".card:not(.old)");
+	nextEvent = nextEvent ? nextEvent : events[events.length - 1];
+	proximosEventos.scrollBy({
+		top: nextEvent.offsetTop - proximosEventos.offsetTop,
+		behavior: "smooth",
 	});
 }
 
